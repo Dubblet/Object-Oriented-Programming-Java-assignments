1ef8862f0756ccd367e92236451e675ba4294860
Steven Bruce <bruces92@gmail.com>
2024-07-01 20:57:54 -0400

    Created initial classes, fields, getters and setters

---
+++ proj1/src/main/java/proj1/DateOfBirth.java
@@ -0,0 +1,33 @@
+package proj1;
+
+public class DateOfBirth {
+    private int month;
+    private int day;
+    private int year;
+
+    public DateOfBirth() {}
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ proj1/src/main/java/proj1/PersonHeartRate.java
@@ -0,0 +1,33 @@
+package proj1;
+
+public class PersonHeartRate {
+    private String firstName;
+    private String lastName;
+    private DateOfBirth dateOfBirth;
+
+    public PersonHeartRate() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public DateOfBirth getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(DateOfBirth dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
