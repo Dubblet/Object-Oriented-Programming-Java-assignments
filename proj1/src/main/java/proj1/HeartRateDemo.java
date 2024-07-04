13f33eab829f8988a72a44d86d7f3c8f07d05509
Steven Bruce <bruces92@gmail.com>
2024-07-04 13:01:49 -0400

    Added calculation methods and built main method for assignment

--- proj1/src/main/java/proj1/HeartRateDemo.java
+++ proj1/src/main/java/proj1/HeartRateDemo.java
@@ -1,4 +1,41 @@
 package proj1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HeartRateDemo {
+    public static void main(String[] args) {
+            ArrayList<PersonHeartRate> heartRateList = new ArrayList<PersonHeartRate>();
+            String continueInput;
+
+            do {
+                Scanner firstNameInput = new Scanner(System.in);
+                System.out.println("Please enter your first name.");
+                String firstNameEntered = firstNameInput.nextLine();
+
+                Scanner lastNameInput = new Scanner(System.in);
+                System.out.println("Please enter your last name.");
+                String lastNameEntered = lastNameInput.nextLine();
+
+                Scanner birthDateInput = new Scanner(System.in);
+                System.out.println("Please enter your date of birth, as all numbers separated by dashes. ie) mm-dd-yyyy");
+                String birthDateEntered = birthDateInput.nextLine();
+
+                DateOfBirth dateOfBirth = new DateOfBirth();
+                String[] birthDateArray = birthDateEntered.split("-");
+                dateOfBirth.setMonth(Integer.parseInt(birthDateArray[0]));
+                dateOfBirth.setDay(Integer.parseInt(birthDateArray[1]));
+                dateOfBirth.setYear(Integer.parseInt(birthDateArray[2]));
+
+                PersonHeartRate personHeartRate = new PersonHeartRate(firstNameEntered, lastNameEntered, dateOfBirth);
+                heartRateList.add(personHeartRate);
+
+                Scanner continuePrompt = new Scanner(System.in);
+                System.out.println("Type \"q\" to quit, or press enter key to continue");
+                continueInput = continuePrompt.nextLine();
+            } while (!continueInput.equalsIgnoreCase("q"));
+            for (PersonHeartRate pHR : heartRateList) {
+                pHR.printData();
+            }
+    }
 }
--- proj1/src/main/java/proj1/PersonHeartRate.java
+++ proj1/src/main/java/proj1/PersonHeartRate.java
@@ -1,11 +1,17 @@
 package proj1;
 
+import java.time.LocalDate;
+
 public class PersonHeartRate {
     private String firstName;
     private String lastName;
     private DateOfBirth dateOfBirth;
 
-    public PersonHeartRate() {}
+    public PersonHeartRate(String firstName, String lastName, DateOfBirth dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -29,5 +35,36 @@ public class PersonHeartRate {
 
     public void setDateOfBirth(DateOfBirth dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public int calculateAge(DateOfBirth dateOfBirth) {
+        LocalDate currentDate = LocalDate.now();
+        int age = currentDate.getYear() - dateOfBirth.getYear();
+        if (currentDate.getMonthValue() < dateOfBirth.getMonth()) {
+            age -= 1;
+        } else if (currentDate.getMonthValue() == dateOfBirth.getMonth()) {
+            if (currentDate.getDayOfMonth() < dateOfBirth.getDay()) {
+                age -= 1;
+            }
+        }
+        return age;
+    }
+
+    public int maxBPM(int age) {
+        return 220 - age;
+    }
+
+    public String bPMRange(int maxBPM) {
+        int lowerRange = (int) (maxBPM * 0.5);
+        int upperRange = (int) (maxBPM * 0.85);
+        return lowerRange + " - " + upperRange;
+    }
+
+    public void printData() {
+        int age = calculateAge(getDateOfBirth());
+        int maxHeartRate = maxBPM(age);
+        System.out.println(getLastName() + ", " + getFirstName());
+        System.out.println("Max heart rate: " + maxHeartRate);
+        System.out.println("Target heart rate: " + bPMRange(maxHeartRate));
     }
 }
