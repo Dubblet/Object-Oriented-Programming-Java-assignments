9d89f4cdb0a7d642c75ba9b7aeed39da35c5bf30
Steven Bruce <bruces92@gmail.com>
2024-07-12 22:33:58 -0400

    Created Abstract Class and child classes, and basic fields and methods

---
+++ proj2/src/main/java/Ghost.java
@@ -0,0 +1,11 @@
+public class Ghost extends Enemy {
+
+    public Ghost(int weight, int height) {
+        super(weight, height);
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("Boo!");
+    }
+}
---
+++ proj2/src/main/java/MonsterAttacks.java
@@ -0,0 +1,5 @@
+public class MonsterAttacks {
+    public static void main(String[] args) {
+
+    }
+}
