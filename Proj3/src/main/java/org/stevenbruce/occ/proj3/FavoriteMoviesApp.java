999dca7c4bb9707c5d12f781663fd2c17afc1e8a
Steven Bruce <bruces92@gmail.com>
2024-08-09 22:12:42 -0400

    Added comments

--- Proj3/src/main/java/org/stevenbruce/occ/proj3/FavoriteMoviesApp.java
+++ Proj3/src/main/java/org/stevenbruce/occ/proj3/FavoriteMoviesApp.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class FavoriteMoviesApp extends Application {
+    // Start method override to open the JavaFX application in a new window.
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FavoriteMoviesApp.class.getResource("favorite-movies-view.fxml"));
--- Proj3/src/main/java/org/stevenbruce/occ/proj3/FavoriteMoviesController.java
+++ Proj3/src/main/java/org/stevenbruce/occ/proj3/FavoriteMoviesController.java
@@ -11,13 +11,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.util.Duration;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 public class FavoriteMoviesController {
+    // FXML IDs
     @FXML
     private ListView listOfMovies;
     @FXML
@@ -25,28 +25,40 @@ public class FavoriteMoviesController {
     @FXML
     private Label deleteBtnReminder;
 
+    // Create a filename to be saved and an ObservableList to be used in updating the ListView.
     File saveFile = new File("movies.txt");
     ObservableList<String> movieList = FXCollections.observableArrayList();
 
+    // Sets the ListView to allow multiple selections. Update it with changes to the observable movieList.
     public void initialize() {
         listOfMovies.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         listOfMovies.setItems(movieList);
     }
 
+    /**
+     * Takes the textfield input and adds it to the Observable movieList, which also updates the ListView.
+     * @param event When the 'Add' button is clicked.
+     */
     @FXML
     protected void onClickAdd(ActionEvent event) {
         movieList.add(txtNameEnter.getText().toLowerCase().trim());
         txtNameEnter.setText("");
     }
 
+    /**
+     * Removes the selected row(s) from the listView of movies.
+     * @param event When the 'Remove' button is clicked.
+     */
     @FXML
     protected void onClickRemove(ActionEvent event) {
         ObservableList<Integer> selectedIndices = listOfMovies.getSelectionModel().getSelectedIndices();
         if (!selectedIndices.isEmpty()) {
+            // Works backwards through the list, and removes the row if its index matches one that was selected.
             for (int i = selectedIndices.size() - 1; i >= 0; i--) {
                 listOfMovies.getItems().remove((int) selectedIndices.get(i));
             }
         } else {
+            // If no rows are selected, make a reminder visible for 3 seconds then hide it again.
             Timeline reminder = new Timeline(
                     new KeyFrame(Duration.ZERO,
                             e -> {
@@ -61,6 +73,11 @@ public class FavoriteMoviesController {
         }
     }
 
+    /**
+     * Creates or overwrites the 'movies.txt' file with what is currently in the ListView of movies.
+     * @param event When the 'Save' button is clicked.
+     * @throws IOException
+     */
     @FXML
     protected void onClickSave(ActionEvent event) throws IOException {
         if (saveFile.exists()) {
@@ -71,6 +88,11 @@ public class FavoriteMoviesController {
         }
     }
 
+    /**
+     * Loads the 'movies.txt' file, replacing with what is currently in the ListView of movies with the file's contents.
+     * @param event When the 'Load' button is clicked.
+     * @throws IOException
+     */
     @FXML
     protected void onClickLoad(ActionEvent event) throws IOException {
         if (saveFile.exists()) {
